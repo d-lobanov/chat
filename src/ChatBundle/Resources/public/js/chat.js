3db7e93da51d4f86d7935397b5a27aecdffb281d
d.lobanov <d.lobanov@itransition.com>
2015-06-19 18:47:10 +0300

    Front-end.

--- src/ChatBundle/Resources/public/js/chat.js
+++ src/ChatBundle/Resources/public/js/chat.js
@@ -1,30 +1,74 @@
-var socket = new WebSocket("ws://chat.dev/websocket");
+var socketUrl = "ws://chat.dev/websocket";
 
-socket.onmessage = function(event) {
-    var incomingMessage = event.data;
-    $('.msg-wrap').append(incomingMessage);
-    $("#dialog-chat").animate({ scrollTop: $("#dialog-chat")[0].scrollHeight}, 500);
+var ChatSocket = function (socketUrl) {
+
+    var connection = new WebSocket(socketUrl);
+
+    connection.onopen = function() {
+        console.log('Connect.');
+    };
+
+    connection.onmessage = function(event) {
+        var response = JSON.parse(event.data);
+        switch (response.head.event){
+            case 'message':
+                console.log('message');
+                this.onEventMessage(response);
+                break;
+            case 'delete':
+                this.onEventDelete(response);
+                break;
+            default:
+                console.log(response.head.event);
+        }
+    };
+
+    connection.onerror = function(error) {
+        console.log('Error: ' + error.message);
+    };
+
+    connection.onclose = function(event) {
+        alert('Connection close');
+        console.log('Code: ' + event.code + ' reason: ' + event.reason);
+    };
+
+    connection.onEventDelete = function(response) {
+        var id = response.body.messageId;
+        var messageBlock = $( '#msg_' + id );
+        messageBlock.addClass('msg-delete');
+        messageBlock.text('Message delete');
+    };
+
+    connection.onEventMessage = function(response) {
+        var message = response.body.messTemplate;
+        $('.msg-wrap').append(message);
+        $("#dialog-chat").animate({ scrollTop: $("#dialog-chat")[0].scrollHeight}, 500);
+    };
+
+    connection.sendObject = function(obj) {
+        data = JSON.stringify(obj);
+        this.send(data);
+    };
+
+    connection.sendMessage = function(roomId, text) {
+        obj = {event: 'message', info: {roomId: roomId, text: text}};
+        this.sendObject(obj);
+    };
+
+    connection.deleteMessage = function(roomId, messageId) {
+        obj = {event: 'delete', info: {roomId: roomId, messageId: messageId}};
+        this.sendObject(obj);
+    };
+
+    return connection;
 };
 
-socket.onopen = function() {
-    console.log('Connect.');
-};
-
-socket.onerror = function(error){
-    console.log(error.message);
-}
-
-socket.onclose = function(event) {
-    alert('Connection close');
-    console.log('Code: ' + event.code + ' reason: ' + event.reason);
-};
-
-
+var socket = new ChatSocket(socketUrl);
 
 $('form[name=publish]').submit(function(){
     var message = this.message.value;
     if(message.replace(/\s/g, '').length > 0){
-        window.socket.send(message);
+        window.socket.sendMessage(1 , message);
     }
     this.message.value = '';
     return false;
@@ -35,4 +79,10 @@ $('form[name=publish] textarea').keypress(function(event) {
         event.preventDefault();
         $('form[name=publish]').submit();
     }
+});
+
+$(document).delegate('.btn-trash', 'click', function(){
+    var messageBlock = this.closest('.media');
+    var messageId = messageBlock.getAttribute('id').replace('msg_', '');
+    window.socket.deleteMessage(1, messageId);
 });
