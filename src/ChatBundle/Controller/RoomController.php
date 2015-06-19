b2dff39693aac56e9b2cdccc57309d64e106cdc7
d.lobanov <d.lobanov@itransition.com>
2015-06-19 20:21:38 +0300

    Routing.

--- src/ChatBundle/Controller/RoomController.php
+++ src/ChatBundle/Controller/RoomController.php
@@ -9,16 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
- * Class DefaultController
+ * Class RoomController
  * @package ChatBundle\Controller
  */
-class DefaultController extends Controller
+class RoomController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
      * @return Response
      */
-    public function indexAction()
+    public function exampleAction()
     {
         $response = new Response('Test');
         $response->headers->setCookie(new Cookie('test', '123'));
@@ -27,11 +26,10 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/room/{name}", name="room")
      * @param string $name
      * @return Response
      */
-    public function roomAction($name)
+    public function showAction($name)
     {
         $roomRepository = $this->getDoctrine()->getRepository('ChatBundle:Room');
         $room = $roomRepository->existsRoomByName($name);
@@ -40,18 +38,35 @@ class DefaultController extends Controller
         }
 
         $user = $this->getUser();
-        $isUser = $roomRepository->isUser($room->getId(), $user->getId());
+        $roomId = $room->getId();
+        $isUser = $roomRepository->isUser($roomId, $user->getId());
         if ($isUser == false) {
             return new Response('Access denied', 403);
         }
 
-        $isModerator = $roomRepository->isModerator($room->getId(), $user->getId());
+        $isModerator = $roomRepository->isModerator($roomId, $user->getId());
         $messages = $roomRepository->getMessagesGroupByDate($room->getId());
         $rooms = $user->getAllRoomsAsArray();
 
         return $this->render('ChatBundle:Default:index.html.twig', array(
             'messages' => $messages,
             'rooms' => $rooms,
+            'currRoom' => $roomId
         ));
+    }
+
+    /**
+     * Homepage, redirect to the first room
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function indexAction()
+    {
+        $rooms = $this->getUser()->getRooms();
+        if ($rooms->isEmpty()) {
+            return new Response('Room not found', 404);
+        }
+
+        $name = $rooms->first()->getName();
+        return $this->redirectToRoute('room_show', array('name' => $name));
     }
 }
